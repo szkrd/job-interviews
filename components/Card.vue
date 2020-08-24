6397f0b0c8705914b7748332e04d61b64403b89a
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2020-08-24 22:09:45 +0200

    card styling, varioud mock cards, primitive persistance for demo page

--- components/Card.vue
+++ components/Card.vue
@@ -1,48 +1,149 @@
 <template>
-  <div class="card-component">
-    <figure class="cover">
-      <img v-if="!coverHeight" src="~/assets/room.jpg" alt="Photo of the room" />
-      <figcaption v-if="coverHeight">Photo of the room</figcaption>
+  <div class="card-component" :class="{grow}">
+    <figure class="cover pull-up" v-if="coverUrl && !coverUnderHeader" :style="coverStyle">
+      <img v-if="!coverHeight" :src="coverUrl" alt="Photo of the room"/>
+      <figcaption v-if="coverHeight && coverDescription">{{ coverDescription }}</figcaption>
     </figure>
-    <header>
-      <div class="thumbnail"></div>
-      <div class="titles">
-        <h2>{{ title }}</h2>
-        <h3>{{ subtitle }}</h3>
+    <header v-if="title || subtitle || thumbnailUrl">
+      <div v-if="thumbnailUrl" class="thumbnail" :style="{ backgroundImage: `url(${thumbnailUrl})` }"></div>
+      <div class="titles" v-if="title || subtitle">
+        <h2 v-if="title">{{ title }}</h2>
+        <h3 v-if="subtitle">{{ subtitle }}</h3>
       </div>
     </header>
-    <main>
+    <figure class="cover" v-if="coverUnderHeader" :style="coverStyle">
+      <!-- TODO maybe? move to component, pull up as prop -->
+      <img v-if="!coverHeight" :src="coverUrl" alt="Photo of the room"/>
+      <figcaption v-if="coverHeight">Photo of the room</figcaption>
+    </figure>
+    <main v-if="hasBody" class="grow">
       <slot name="body"></slot>
     </main>
-    <footer>
+    <footer v-if="hasFooter" :class="{ 'pull-up': !hasBody, borderless: borderlessFooter }">
       <slot name="footer"></slot>
     </footer>
   </div>
 </template>
-<style>
+<style lang="scss" scoped>
 .card-component {
   font-family: 'Roboto', sans-serif;
+  border-radius: 4px;
+  border: 1px solid rgba(45, 45, 45, 0.2);
+  padding: 24px 0 0 0;
+  background-color: #fff;
+  color: #2D2D2D;
+  display: flex;
+  flex-direction: column;
 }
 
-h2 {
-  font-family: 'Lora', serif;
+.cover {
+  margin: 0 0 24px 0;
+  padding: 0;
+  background-position: center;
+  background-size: cover;
+
+  img {
+    width: 100%;
+  }
+
+  figcaption {
+    display: none;
+  }
+}
+
+header {
+  margin: 0 24px 24px 24px;
+  display: flex;
+  align-items: center;
+
+  .thumbnail {
+    background-position: center;
+    background-size: cover;
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    margin: 0 24px 0 0;
+  }
+
+  h2 {
+    font-family: 'Lora', serif;
+    margin: 0;
+    padding: 0;
+    font-size: 20px;
+    line-height: 28px;
+  }
+
+  h3 {
+    color: #6E6E6E;
+    font-size: 14px;
+    line-height: 20px;
+    margin: 0;
+    padding: 0;
+  }
+}
+
+main {
+  font-size: 16px;
+  line-height: 20px;
+  margin: 0 24px 24px 24px;
+}
+
+footer {
+  padding: 24px;
+  color: #6E6E6E;
+  font-size: 14px;
+  line-height: 20px;
+  border-top: 1px solid rgba(45, 45, 45, 0.2);
+}
+
+// utility classes
+
+.pull-up {
+  margin-top: -24px;
+}
+
+.borderless {
+  border: 0;
+}
+
+.grow {
+  flex-grow: 1;
 }
 </style>
 <script>
 module.exports = {
   props: {
-    // image url
-    cover: String,
+    coverUrl: String,
     // leave unset for ratio TODO
     coverHeight: Number,
     // by default it's above, the first element inside the card TODO
     coverUnderHeader: Boolean,
-    // image url
-    thumbnail: String,
+    coverDescription: String,
+    thumbnailUrl: String,
     title: String,
     subtitle: String,
     isLoading: Boolean,
-    onClick: Function
+    onClick: Function,
+    // adds flex grow to the root node
+    grow: Boolean
+  },
+  computed: {
+    hasBody () {
+      return !!this.$slots.body;
+    },
+    hasFooter () {
+      return !!this.$slots.footer;
+    },
+    coverStyle () {
+      return {
+        height: this.coverHeight ? `${this.coverHeight}px` : 'auto',
+        backgroundImage: `url(${this.coverUrl})`
+      };
+    },
+    // image size ratio may leave a couple of stray pixels under the picture
+    borderlessFooter () {
+      return this.coverUnderHeader && !this.hasBody;
+    }
   }
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,222 @@
 <template>
-  <div>
-    <h1>Hello world!</h1>
-    <Card
-        title="Card title"
-        subtitle="Card subtitle"
-    >
-      <div slot="body">
-        body
+  <div class="root-component" :class="{'width-flipped': widthFlipped, zoomed}">
+    <h1>Card anatomy</h1>
+    <p class="controls" @click="persist">
+      <button @click="changeZoom">zoom in/out</button>
+      <button @click="changeCardWidths">change width</button>
+      <button @click="changeGridMode">grid mode</button>
+    </p>
+    <div class="card-boxes">
+      <div class="card-box">
+        <Card
+            title="Card title"
+            subtitle="Card subtitle"
+            cover-url="/_nuxt/assets/room.jpg"
+            cover-description="air conditioned room with a beautiful lakeside view"
+            thumbnail-url="/_nuxt/assets/room.jpg"
+            :grow="gridMode"
+        >
+          <div slot="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et
+          </div>
+          <div slot="footer">
+            Card footer
+          </div>
+        </Card>
       </div>
-    </Card>
+
+      <!-- region variation: no title -->
+      <div class="card-box">
+        <Card
+            cover-url="/_nuxt/assets/room.jpg"
+            :grow="gridMode"
+        >
+          <div slot="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et
+          </div>
+          <div slot="footer">
+            Card footer
+          </div>
+        </Card>
+      </div>
+      <!-- endregion -->
+
+      <!-- region variation: cover under image -->
+      <div class="card-box">
+        <Card
+            title="Card title"
+            :cover-under-header="true"
+            subtitle="Card subtitle"
+            cover-url="/_nuxt/assets/room.jpg"
+            thumbnail-url="/_nuxt/assets/room.jpg"
+            :grow="gridMode"
+        >
+          <div slot="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et
+          </div>
+          <div slot="footer">
+            Card footer
+          </div>
+        </Card>
+      </div>
+      <!-- endregion -->
+
+      <!-- region variation: no body -->
+      <div class="card-box">
+        <Card
+            title="Card title"
+            :cover-under-header="true"
+            subtitle="Card subtitle"
+            cover-url="/_nuxt/assets/room.jpg"
+            thumbnail-url="/_nuxt/assets/room.jpg"
+            :grow="gridMode"
+        >
+          <div slot="footer">
+            Card footer
+          </div>
+        </Card>
+      </div>
+      <!-- endregion -->
+
+      <!-- region variation: only body and footer -->
+      <div class="card-box">
+        <Card :grow="gridMode">
+          <div slot="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et
+          </div>
+          <div slot="footer">
+            Card footer
+          </div>
+        </Card>
+      </div>
+      <!-- endregion -->
+
+      <!-- region variation: only title, thumbnail and body -->
+      <div class="card-box">
+        <Card
+            title="Card title"
+            thumbnail-url="/_nuxt/assets/room.jpg"
+            :grow="gridMode"
+        >
+          <div slot="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et
+          </div>
+        </Card>
+      </div>
+      <!-- endregion -->
+
+      <!-- region variation: fixed cover height, no thumbnail -->
+      <div class="card-box">
+        <Card
+            title="Card title"
+            subtitle="Card subtitle"
+            cover-url="/_nuxt/assets/room.jpg"
+            :cover-height="100"
+            :grow="gridMode"
+        >
+          <div slot="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et
+          </div>
+          <div slot="footer">
+            Card footer
+          </div>
+        </Card>
+      </div>
+      <!-- endregion -->
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.root-component {
+  padding: 40px;
+
+  &.width-flipped .card-box {
+    width: 400px;
+  }
+
+  &.zoomed .card-boxes {
+    transform: scale(.5);
+    transform-origin: top left;
+    width: 200%;
+  }
+}
+
+h1 {
+  font-family: 'Lora', serif;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 56px;
+  margin: 0 0 40px 0;
+  padding: 0;
+}
+
+.controls {
+  padding: 0 0 24px 0;
+  margin: 0;
+}
+
+.card-boxes {
+  box-shadow: 0 0 5px inset rgba(0, 0, 255, .3);
+  border-radius: 4px;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card-box {
+  box-shadow: 0 0 5px inset rgba(255, 0, 0, .2);
+  border-radius: 4px;
+  padding: 5px;
+  width: 320px;
+  min-height: 600px;
+  transition: all .3s;
+  display: flex;
+  flex-direction: column;
+}
+</style>
+<script>
+// probably there's a much much nicer way to do this with nuxt and vuex,
+// not to mention the fact that quota errors should really be handled
+// at least with try-catches, but with the nuxt style auto reloader being a
+// broken mess, I still have to press refresh way too often...
+const LS_KEY = 'kasa-entry-state';
+const storageState = process.browser ? JSON.parse(localStorage.getItem(LS_KEY) || '{}') : {};
+const persistable = ['zoomed', 'gridMode', 'widthFlipped'];
+
+module.exports = {
+  data: () => ({
+    zoomed: false,
+    gridMode: false,
+    widthFlipped: false
+  }),
+  mounted () {
+    if (process.browser) {
+      persistable.forEach(key => this[key] = storageState[key] || this[key]);
+    }
+  },
+  methods: {
+    changeCardWidths () {
+      this.widthFlipped = !this.widthFlipped;
+    },
+    changeGridMode () {
+      this.gridMode = !this.gridMode;
+    },
+    changeZoom () {
+      this.zoomed = !this.zoomed;
+    },
+    persist () {
+      if (process.browser) {
+        localStorage.setItem(LS_KEY, JSON.stringify(
+            persistable.reduce((acc, key) => ({ ...acc, [key]: this[key] }), {})
+        ));
+      }
+    }
+  }
+};
+</script>
