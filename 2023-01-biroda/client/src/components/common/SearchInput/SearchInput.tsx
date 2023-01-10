7efe80b35751039b2d747a41b71d6d472c406984
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-01-10 10:22:58 +0100

    scss hacking for the search input

--- 2023-01-biroda/client/src/components/common/SearchInput/SearchInput.tsx
+++ 2023-01-biroda/client/src/components/common/SearchInput/SearchInput.tsx
@@ -1,18 +1,24 @@
-import React, { FC, HTMLProps } from 'react';
+import { Search as SearchIcon } from 'react-feather';
 import classNames from 'classnames';
+import React, { FC, HTMLProps } from 'react';
 import styles from './SearchInput.module.scss';
+import scssVars from '../../../styles/scssVars';
 
 interface ISearchInputProps extends HTMLProps<HTMLInputElement> {
+  inputClassName?: string;
   theme?: 'default' | 'header';
 }
 
 const SearchInput: FC<ISearchInputProps> = (props) => {
   return (
-    <input
-      type="search"
-      {...props}
-      className={classNames(styles.searchInput, styles[props.theme ?? 'default'])}
-    />
+    <div className={classNames(styles.searchInput, props.className)}>
+      <SearchIcon stroke={scssVars.colors.aluminiumHighlight} />
+      <input
+        type="search"
+        {...props}
+        className={classNames(styles[props.theme ?? 'default'], props.inputClassName)}
+      />
+    </div>
   );
 };
 
