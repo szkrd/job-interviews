8a575dcbecea7665b99694ed2335ff7b8e50e592
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-01-13 14:02:36 +0100

    better api call error handling

---
+++ 2023-01-biroda-vue/client/src/components/CenterErrorMessage.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-typography-paragraph class="h-full flex place-content-center text-center">
+    <a-typography-text type="danger">
+      <slot>
+        We're sorry, something unexpected happened.<br />Please try again later.
+      </slot>
+    </a-typography-text>
+  </a-typography-paragraph>
+</template>
