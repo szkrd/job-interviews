b3ac7464f21571b7ea96fc5c9f15ffbfd1f81b14
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-01-16 14:04:38 +0100

    vite config changes

--- 2023-01-biroda-vue/client/vite.config.ts
+++ 2023-01-biroda-vue/client/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // chunk splitting only splits the js code, not the css unfortunately
+  plugins: [vue(), splitVendorChunkPlugin()],
+  build: {
+    // css code splitting is true by default, but it's only for async css?
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 2000,
+  },
   server: {
     port: 4000,
   },
