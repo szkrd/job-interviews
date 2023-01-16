256662a00c1965708553f6284f7d0719646867b4
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-01-16 13:22:01 +0100

    add better movie details to api

--- 2022-04-biroda/server/routes/getMovieById.mjs
+++ 2022-04-biroda/server/routes/getMovieById.mjs
@@ -4,6 +4,13 @@ import { wikipediaApi } from '../modules/wikipediaApi.mjs';
 
 const TMDB_IMAGE_PATH_LARGE = 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2';
 
+function getImagePath(path = '') {
+  if (path && typeof path === 'string') {
+    return TMDB_IMAGE_PATH_LARGE + path;
+  }
+  return '';
+}
+
 /**
  * Get movie data (from Wikipedia and TMDB)
  * @param {import('express').Request} req
@@ -12,6 +19,7 @@ const TMDB_IMAGE_PATH_LARGE = 'https://www.themoviedb.org/t/p/w300_and_h450_best
  */
 export const routeGetMovieById = async (req, res, next) => {
   const { id } = req.params;
+  const detailed = ['1', 'true'].includes(req.query.detailed);
 
   // invalid movie id, bail out with 400
   if (!id || !/^\d+$/.test(id)) {
@@ -51,12 +59,46 @@ export const routeGetMovieById = async (req, res, next) => {
     overviewSource = 'tmdb';
   }
 
+  let details = {};
+  if (detailed) {
+    details = {
+      adult: movie.adult ?? false,
+      budget: movie.budget ?? -1,
+      revenue: movie.revenue ?? -1,
+      genres: movie.genres || [],
+      homepage: movie.homepage || '',
+      runTime: movie.runtime ?? -1,
+      status: movie.status, // Rumored, Planned, In Production, Post Production, Released, Canceled
+      tagLine: movie.tagLine || '',
+      video: movie.video ?? false,
+      score: movie.vote_average * 10,
+    };
+    if (movie.belongs_to_collection) {
+      const data = movie.belongs_to_collection;
+      details.collection = {
+        id: data.id,
+        name: data.name,
+        poster: getImagePath(data.poster_path),
+        backdrop: getImagePath(data.backdrop_path),
+      };
+    }
+    if (movie.production_companies) {
+      details.productionCompanies = movie.production_companies.map((company) => ({
+        id: company.id,
+        logo: getImagePath(company.logo),
+        name: company.name,
+        country: company.originCountry,
+      }));
+    }
+  }
+
   res.send({
+    ...details,
     id: movie.id,
     title,
     releaseDate: movie.release_date || '',
-    poster: movie.poster_path ? TMDB_IMAGE_PATH_LARGE + movie.poster_path : '',
-    backdrop: movie.backdrop_path ? TMDB_IMAGE_PATH_LARGE + movie.backdrop_path : '',
+    poster: getImagePath(movie.poster_path),
+    backdrop: getImagePath(movie.backdrop_path),
     overview: wiki.firstParagraph || wiki.teaserSnippet || movie.overview || '',
     overviewSource,
     wikipediaUrl: wiki.canonicalUrl || '',
